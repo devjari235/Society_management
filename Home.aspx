c3b6c8607f170f717b2dd7e2e81a9f11302d1a28
devjari235 <devjari235@gmail.com>
2025-04-22 15:18:22 +0530

    all

---
+++ Home.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/H_master.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Society_management.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ Home.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Society_management
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(strcon);
+            if (con.State != System.Data.ConnectionState.Open)
+            {
+                con.Open();
+                Response.Write("Connection Successfully");
+            }
+            else
+            {
+                Response.Write("Fail");
+            }
+        }
+    }
+}
