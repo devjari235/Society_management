2479342153d90341c3266b80a6783d63cf3a5355
devjari235 <devjari235@gmail.com>
2025-04-29 11:49:35 +0530

    login

--- R_Society.aspx.designer.cs
+++ R_Society.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace Society_management
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// ddlAdmin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlAdmin;
+
+        /// <summary>
         /// txtSname control.
         /// </summary>
         /// <remarks>
