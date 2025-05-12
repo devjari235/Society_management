a0f1d783b64d2304ae3c6611606c4c06f21eca76
devjari235 <devjari235@gmail.com>
2025-05-12 17:20:11 +0530

    gtwdfe7d

---
+++ Committee_Details.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Society_management
+{
+    public partial class Committee_Details : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["id"] != null)
+                {
+                    int noticeId;
+                    if (int.TryParse(Request.QueryString["id"], out noticeId))
+                    {
+                        LoadNoticeDetails(noticeId);
+                    }
+                   
+                }
+            }
+        }
+
+        private void LoadNoticeDetails(int id)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                //string query = @"SELECT c.Title, n.Description, n.Expiry_date, n.File_path, 
+                //                    n.Importance, n.Status, n.Posted_date, a.Name 
+                //             FROM tblNotices n 
+                //             INNER JOIN tblAdmin a ON n.admin_id = a.admin_id 
+                //             WHERE n.Notice_id = @id";
+                //  string query = "SELECT c.*,u.User_name from tblCommitteeMember c join tblUser u on u.User_id=c.User_id join tblOwner o on o.Owner_id=u.Owner_id join tblblock b on  b.Block_id = o.Block_id JOIN tblSociety s ON s.Society_id = b.Society_id join tblAdmin a on s.admin_id=a.admin_id where c.Committee_id=@id";
+                string query = @"SELECT 
+    c.Designation,
+    c.From_Date,
+    c.To_date,
+    c.Role,
+    c.Status,
+    c.Block_name,
+    c.Flat_no,
+    c.Email,
+    c.Phone_no,
+    u.User_name 
+FROM tblCommitteeMember c
+JOIN tblUser u ON u.User_id = c.User_id
+JOIN tblOwner o ON o.Owner_id = u.Owner_id
+JOIN tblBlock b ON b.Block_id = o.Block_id
+JOIN tblSociety s ON s.Society_id = b.Society_id
+JOIN tblAdmin a ON s.admin_id = a.admin_id
+WHERE c.Committee_id = @id";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    lblname.Text = dr["User_name"].ToString();
+                    lbldes.Text = dr["Designation"].ToString();
+                    lblRole.Text = dr["Role"].ToString();
+                    lblEmail.Text = dr["Email"].ToString();
+                    lblphone.Text = dr["Phone_no"].ToString();
+                    lblblock.Text = dr["Block_name"].ToString();
+                    lblNo.Text = dr["Flat_no"].ToString();
+                    lblFdate.Text = Convert.ToDateTime(dr["From_Date"]).ToString("dd MMM yyyy");
+                    lblTdate.Text = Convert.ToDateTime(dr["To_date"]).ToString("dd MMM yyyy");
+                    lblStatus.Text = dr["Status"].ToString();
+                    pnlNotice.Visible = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ View_CommiteeMember.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Adashboard.Master" AutoEventWireup="true" CodeBehind="View_CommiteeMember.aspx.cs" Inherits="Society_management.View_CommiteeMember" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+        <script type="text/javascript">
+
+$(document).ready(function () {
+    $(".table").prepend($("<thead></thead>").append($(this).find("tr:first"))).dataTable();
+}) 
+
+        </script>
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+<link href="fontawesome\css\all.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="BreadcrumbContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="PageTitleContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="MainContent" runat="server">
+        <div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <asp:Panel CssClass="alert alert-success" role="alert" ID="Panel1" runat="server" Visible="false">
+                <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            </asp:Panel>
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <div class="table-responsive">
+                <asp:GridView class="table table-striped table-bordered" ID="gvDisplay" runat="server" AutoGenerateColumns="False" OnRowCommand="gvDisplay_RowCommand">
+     <Columns>
+         
+         <asp:TemplateField HeaderText="Committee Member ">
+             <ItemTemplate>
+               <asp:Label Text='<%#Eval("User_name") %>' runat="server"></asp:Label>
+             </ItemTemplate>
+         </asp:TemplateField>
+         <asp:TemplateField HeaderText="Designation">
+             <ItemTemplate>
+                 <asp:Label Text='<%#Eval("Designation") %>' runat="server"></asp:Label>
+             </ItemTemplate>
+         </asp:TemplateField>
+         <asp:TemplateField HeaderText="Phone Number">
+             <ItemTemplate>
+                 <asp:Label Text='<%#Eval("Phone_no") %>' runat="server"></asp:Label>
+             </ItemTemplate>
+         </asp:TemplateField>
+         <asp:TemplateField HeaderText="Role">
+             <ItemTemplate>
+                 <asp:Label Text='<%#Eval("Role") %>' runat="server"></asp:Label>
+             </ItemTemplate>
+         </asp:TemplateField>         
+         <asp:TemplateField HeaderText="Action">
+    <ItemTemplate>
+       <asp:Button ID="btnView" runat="server" Text="View" CommandName="ViewNotice" CommandArgument='<%# Eval("Committee_id") %>' Style="background-color:aquamarine; color:black" />
+    </ItemTemplate>
+</asp:TemplateField>
+     </Columns>
+ </asp:GridView>
+            </div>
+
+        </div>
+    </div>
+</div>
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="ScriptsContent" runat="server">
+</asp:Content>
