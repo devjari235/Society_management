21581637747d532ff854d65498b70dee37ce48b6
devjari235 <devjari235@gmail.com>
2025-05-14 16:49:18 +0530

    gdbhncjd

--- UserDashboard.aspx.cs
+++ UserDashboard.aspx.cs
@@ -112,13 +112,6 @@ namespace Society_management
                                WHERE Expiry_date < GETDATE() AND Status != 'Expired'";
                 SqlCommand updateCmd = new SqlCommand(updateQuery, conn);
                 updateCmd.ExecuteNonQuery();
-
-                // Step 2: Fetch notices
-                //string selectQuery = @"SELECT Notice_id, Title, Description, Expiry_date, File_path, Importance, Status 
-                //               FROM tblNotices 
-                //               WHERE Expiry_date IS NULL OR Expiry_date >= GETDATE() 
-                //               ORDER BY Posted_date DESC";
-
                 string selectQuery = "SELECT n.Notice_id, n.Title, n.Description, n.Expiry_date, n.File_path, n.Importance, n.Status,  n.Posted_date,  a.name FROM tblNotices n INNER JOIN tblAdmin a ON n.admin_id = a.admin_id WHERE n.Expiry_date IS NULL OR n.Expiry_date >= GETDATE() ORDER BY  n.Posted_date DESC";
                 SqlCommand cmd = new SqlCommand(selectQuery, conn);
               //  cmd.Parameters.AddWithValue("@id", Session["A_id"]);
