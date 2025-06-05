16a2c4e934fd3939de9c2ffaafc0a9affadc92f7
harsh47143 <harshgilitwala7@gmail.com>
2025-06-05 16:22:24 +0530

    asdfgh

--- Accounting.aspx.designer.cs
+++ Accounting.aspx.designer.cs
@@ -105,6 +105,15 @@ namespace Society_management
         protected global::System.Web.UI.WebControls.Repeater rptIncome;
 
         /// <summary>
+        /// pnlNoIncome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlNoIncome;
+
+        /// <summary>
         /// rptIncomeSummary control.
         /// </summary>
         /// <remarks>
