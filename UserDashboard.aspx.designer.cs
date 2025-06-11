3e3f51aed0a83afc2be089280b1e0f3df36315f3
devjari235 <devjari235@gmail.com>
2025-06-11 14:40:32 +0530

    gvydt

--- UserDashboard.aspx.designer.cs
+++ UserDashboard.aspx.designer.cs
@@ -258,6 +258,15 @@ namespace Society_management
         protected global::System.Web.UI.WebControls.HyperLink lnkVotePoll;
 
         /// <summary>
+        /// lnkBookFacility control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkBookFacility;
+
+        /// <summary>
         /// rptNotices control.
         /// </summary>
         /// <remarks>
